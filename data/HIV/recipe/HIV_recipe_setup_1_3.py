7cb6ab9622ff6c25a082cc9cebc8189100bf6f61
ludovic.autin@gmail.com <ludovic.autin@gmail.com@47532b5d-7e71-5f9c-68e6-994300363da4>
2012-12-28 23:38:59 +0000

    back to normal
    git-svn-id: https://autofill.googlecode.com/svn@102 47532b5d-7e71-5f9c-68e6-994300363da4

--- data/HIV/recipe/HIV_recipe_setup_1_3.py
+++ data/HIV/recipe/HIV_recipe_setup_1_3.py
@@ -90,8 +90,8 @@ if helper is None :
 #if AutoFill.helper.host == 'c4d' or AutoFill.helper.host == 'maya' :
 #    modeltype = "fbx"#"fbx"#"dae" #fbx
 #else :
-modelFormat = "dae"
-modeltype = "dae"
+modelFormat = "fbx"
+modeltype = "fbx"
 hext= AutoFill.helper.hext
 if AutoFill.helper.host == 'c4d' or AutoFill.helper.host.find("blender") != -1 :
     modelFormat = "dae"
