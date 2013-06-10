0e21db3aba1aead2f3d82828af7c16f71130fef3
Jacek Nowosielski <jacek.nowosielski@wp.pl>
2013-06-10 19:23:26 +0200

    Restructure directories to src/ test/ recipes/, cmake script modified to automatically include all files from src/*

---
+++ cAutoPACK/test/test.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MAIN
+#if !defined( WIN32 )
+    #define BOOST_TEST_DYN_LINK
+#endif
+#include <boost/test/unit_test.hpp>
+
+//Example test
+BOOST_AUTO_TEST_CASE( la ) {
+    BOOST_CHECK_EQUAL(1, 1);
+}
