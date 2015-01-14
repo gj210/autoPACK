68d245429a71adc3d7e00450ba4c570555fd0914
Ludovic Autin <ludovic.autin@gmail.com>
2015-01-13 16:32:39 -0800

    correct json access

--- autopack/scripts/testinstance.py
+++ autopack/scripts/testinstance.py
@@ -28,8 +28,9 @@ except :
     import json
     from json import encoder
 
-f=open("/Users/ludo/Library/Application Support/autoPACK/cache_results/HIV_16_result.json","r")
+#f=open("/Users/ludo/Library/Application Support/autoPACK/cache_results/HIV_16_result.json","r")
 #f=open("/home/ludo/.autoPACK/cache_results/HIV_16_result.json","r")
+f=open("/home/ludo/.autoPACK/cache_results/HIV-1_0.1.6_3.apr.json","r")
 data = json.load(f)
 f.close()
 
@@ -58,38 +59,45 @@ def makeQuad(vector):
     q=helper.createsNmesh("quad"+str(axe),quad,[],f,color=[1,0,0])
     return q
     
-def testOneIngredient(name,oname,principalVector):
-    helper.read("/Users/ludo/Library/Application Support/autoPACK/cache_geometries/"+name+".dae")
-    obj = helper.getObject(name)
-    print (obj,name)
+def testOneIngredient(name,iname,oname,principalVector):
+    helper.read("/home/ludo/.autoPACK/cache_geometries/"+name+".dae")
+#    helper.read("/Users/ludo/Library/Application Support/autoPACK/cache_geometries/"+name+".dae")
+    obj = helper.getObject(iname)
+    print (obj,name,iname)
     #blender
     axis=numpy.array(principalVector[:])#helper.ApplyMatrix([principalVector,],mY) [0]
     mY=helper.rotation_matrix(-math.pi/2.0,[0.0,1.0,0.0])   
     if helper.host.find("blender") !=-1:
-        mA=helper.rotation_matrix(-math.pi/2.0,[1.0,0.0,0.0])
-        mB=helper.rotation_matrix(math.pi/2.0,[0.0,0.0,1.0])    
-        print ("principalVector b",principalVector)
-        #axis = helper.rotatePoint(principalVector,[0.,0.,0.],[0.0,1.0,0.0,-math.pi/2.0])
-        #print ("principalVector b",principalVector,axis)
-        m=matrix(mA)*matrix(mB)
-        helper.setObjectMatrix(obj,matrice=m)
+#        mA=helper.rotation_matrix(-math.pi/2.0,[1.0,0.0,0.0])
+#        mB=helper.rotation_matrix(math.pi/2.0,[0.0,0.0,1.0])    
+#        print ("principalVector b",principalVector)
+#        #axis = helper.rotatePoint(principalVector,[0.,0.,0.],[0.0,1.0,0.0,-math.pi/2.0])
+#        #print ("principalVector b",principalVector,axis)
+#        m=matrix(mA)*matrix(mB)
+#        helper.setObjectMatrix(obj,matrice=m)
+        helper.resetTransformation(obj)
 #    axis=helper.ApplyMatrix([principalVector,],mY) [0]
     elif helper.host=="maya":
-        helper.rotateObj(obj,[0.0,-math.pi/2.0,0.0])
-    print ("rotated ",helper.ApplyMatrix([principalVector,],mY) [0])
+         helper.resetTransformation(obj)
+#        helper.rotateObj(obj,[0.0,-math.pi/2.0,0.0])
+#    print ("rotated ",helper.ApplyMatrix([principalVector,],mY) [0])
     inst1 = helper.newInstance(oname,obj)
-    res2=numpy.array(data['HIV1_envelope_Pack_145_0_2_0_surfaceRecipe']["HIV1_envelope_Pack_145_0_2_0_surf__"+name]["results"])
+#    print (iname)
+#    print (data['compartments']['HIV1_envelope_Pack_145_0_2_0']['surface']['ingredients'].keys())
+    res2=numpy.array(data['compartments']['HIV1_envelope_Pack_145_0_2_0']['surface']['ingredients'][iname]["results"])
     listM=[oneMat(d[0],d[1]) for d in res2]
     print ("principalVector",axis)
     helper.instancePolygon("instOf"+oname, matrices=listM, mesh=inst1,
                                    axis=axis,transpose=True)
     
 
-testOneIngredient("HIV1_MA_Hyb_0_1_0","MA",[0,1,0])     #=> Z 90
-testOneIngredient("HIV1_ENV_4nco_0_1_1","ENV",[0,0,-1]) #=> X 90
+testOneIngredient("HIV1_MA_Hyb_0_1_0","HIV1_MA_Hyb_0_1_0","MA",[0,1,0])     #=> Z 90
+testOneIngredient("HIV1_ENV_4nco_0_1_2_Left","HIV1_ENV_4nco_0_1_1","ENV",[0,0,-1]) #=> X 90
 #makeQuad([0,0,-1]) 
-testOneIngredient("HIV1_NEF_Hyb_0_2_0","Nef",[0,0,1]) 
+testOneIngredient("HIV1_NEF_Hyb_0_2_0","HIV1_NEF_Hyb_0_2_0","Nef",[0,0,1]) 
 #testOneIngredient("HIV1_ENV_4nco_0_1_1","ENV",[0,0,-1]) 
-testOneIngredient("HIV1_HLA_1dlh_0_1_0","hla",[0,0,-1]) 
+testOneIngredient("HIV1_HLA_1dlh_0_1_0","HIV1_HLA_1dlh_0_1_0","hla",[0,0,-1]) 
 
 ##execfile("/Users/ludo/DEV/autoPACK_github/autopack/scripts/testinstance.py")
+##execfile("/opt/data/dev/autoPACK/autopack/scripts/testinstance.py")
+#exec(open("/opt/data/dev/autoPACK/autopack/scripts/testinstance.py").read())
